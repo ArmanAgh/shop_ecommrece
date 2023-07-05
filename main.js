e591c7b5d61dc486df50c2a7f71d227c9cbb0943
Arman <arman2022ex@gmail.com>
2023-07-06 02:43:28 +0330

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const header = document.querySelector('header')
+
+window.addEventListener('scroll' , function(){
+    header.classList.toggle('sticky' , this.window.scrollY>0)
+})
+
+
+
+let menu = document.getElementById('#menu-icon')
+let navmenu = document.getElementById('.navmenu')
+
+menu.onclick = ()=>{
+    menu.classList.toggle('bx-x')
+    navmenu.classList.toggle('open')
+}
